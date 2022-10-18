394aa972dea79e87f0545b0514507fe774f2b72e
gabrielqueiroz08 <100386457+gabrielqueiroz08@users.noreply.github.com>
2022-10-18 20:13:44 -0300

    archer

--- sketch.js
+++ sketch.js
@@ -6,12 +6,13 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var canvas;
 var palyer, playerBase, playerArcher;
-var baseimage;
+var baseImage;
+var playerImage;
 
 function preload() {
   backgroundImg = loadImage("./assets/background.png");
-  baseimage = loadImage("./assets/base.png");
-  playerimage = loadImage("./assets/player.png");
+  baseImage = loadImage("./assets/base.png");
+  playerImage = loadImage("./assets/player.png");
 }
 
 function setup() {
@@ -20,25 +21,30 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
   angleMode(DEGREES);
-
+  var options = {
+    isStatic: true
+  }
   //criar corpo da base do jogador
-
+  playerBase = Bodies.rectangle(200,350,180,150,options);
+  World.add(world, playerBase);
   //criar corpo do jogador
-
-
+  player = Bodies.rectangle(250,playerBase.position.y-160, 50,180,options);
+  World.add(world, player);
 
 }
 
 function draw() {
   background(backgroundImg);
+  Engine.update(engine);
 
   //exibir a imagem do jogador usando a função image()
-
+  image(baseImage, playerBase.position.x, playerBase.position.y, 180, 150);
+  image(playerImage, player.position.x, player.position.y, 50, 180);
 
   //exibir a imagem da base do jogador usando a função image()
 
 
-  Engine.update(engine);
+ 
 
   // Title
   fill("#FFFF");
